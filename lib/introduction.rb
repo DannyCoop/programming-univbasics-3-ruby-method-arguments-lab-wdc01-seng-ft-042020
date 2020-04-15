ffd441cf1c8e465eb6dbc579ab52f8b416a14bd1
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-15 01:17:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,23 @@
+name = "Daniel"
+lang = "js"
+
 def introduction name
   puts "Hi, my name is #{name}."
 end
 
-introduction bill
+introduction name
 
 def introduction_with_language name, language
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language name, lang
 
+def introduction_with_language_optional name, language="Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional name
 
 
 
